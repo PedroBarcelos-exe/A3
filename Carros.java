6ea5a181cc7d4a4a7353fccf0dfa259ac80162f4
PedroBarcelos-exe <pedromutube@gmail.com>
2025-05-19 19:48:00 -0300

    Add files via upload

---
+++ Carros.java
@@ -0,0 +1,7 @@
+public class Carros {
+    private Carros[] carros = new Carros[100];
+    private int id;
+    private String marca;
+    private String cor;
+    private int ano;
+}
---
+++ Pessoas.java
@@ -0,0 +1,8 @@
+public class Pessoas{
+    private Pessoas[] pessoas = new Pessoas[100];
+    private int id;
+    private String nome;
+    private double CPF;
+    private double dataNascimento;
+    private char genero;
+}
