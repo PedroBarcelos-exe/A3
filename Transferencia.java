f044583b69c2ed8c7050b0394b3e0b15a859088a
PedroBarcelos-exe <pedromutube@gmail.com>
2025-06-05 13:53:44 +0000

    Transferencia

--- Transferencia.java
+++ Transferencia.java
@@ -2,23 +2,37 @@ package modelo;
 
 import java.time.LocalDate;
 
+package modelo;
+
+import java.time.LocalDate;
+
 public class Transferencia {
     private int id;
     private int idVeiculo;
     private int idPessoaCompra;
-    private Integer idPessoaVenda; // pode ser null
+    private Integer idPessoaVenda;
     private String placaAnterior;
     private String placaAtual;
     private String statusTransferencia;
     private LocalDate data;
 
-    public Transferencia(int idVeiculo, int idPessoaCompra, Integer idPessoaVenda, String placaAnterior, String placaAtual, LocalDate data) {
+
+    public Transferencia(int idVeiculo, int idPessoaCompra, Integer idPessoaVenda,
+                         String placaAnterior, String placaAtual, LocalDate data) {
         this.idVeiculo = idVeiculo;
         this.idPessoaCompra = idPessoaCompra;
         this.idPessoaVenda = idPessoaVenda;
         this.placaAnterior = placaAnterior;
         this.placaAtual = placaAtual;
         this.data = data;
-        this.statusTransferencia = null;
+        this.statusTransferencia = "N"; 
+    }
+
+    public Transferencia(int id, int idVeiculo, int idPessoaCompra, Integer idPessoaVenda,
+                         String placaAnterior, String placaAtual, String statusTransferencia,
+                         LocalDate data) {
+        this(idVeiculo, idPessoaCompra, idPessoaVenda, placaAnterior, placaAtual, data);
+        this.id = id;
+        this.statusTransferencia = statusTransferencia;
     }
 }
